0cb9acbefd2965cc4cf0988b095d869bbf78a277
nikitagorojanin <ngorojanin@gmail.com>
2021-08-05 16:44:35 +0500

    changes in models

--- api/manager/account.py
+++ api/manager/account.py
@@ -14,12 +14,14 @@ class AccountManager:
         print('Run add account with {} rows'.format(len(accounts_data)))
 
         for i, account_info in enumerate(accounts_data):
+            log.debug('Add account with data: {}'.format(account_info))
+
             if 'payload' not in account_info:
                 continue
 
             payload = account_info.get('payload', {})
             phone = payload.get('phone_number')
-            id_1c = payload.get('id_1c')
+            id_1c = payload.get('id')
 
             if not phone or not id_1c:
                 continue
