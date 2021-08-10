c1a1888792bcefeaa38917a75f23f724f4cc3030
nikitagorojanin <ngorojanin@gmail.com>
2021-08-10 15:20:16 +0500

    changes in models

--- branch/form.py
+++ branch/form.py
@@ -4,9 +4,9 @@ from api.models import Branche
 
 
 class BranchForm(forms.ModelForm):
-    address_rus = forms.CharField(label='Текст на русском', widget=forms.Textarea)
-    address_uzb = forms.CharField(label='Текст на узбекском', widget=forms.Textarea, required=False)
-    address_eng = forms.CharField(label='Текст на английском', widget=forms.Textarea, required=False)
+    address_rus = forms.CharField(label='Адрес на русском', widget=forms.Textarea)
+    address_uzb = forms.CharField(label='Адрес на узбекском', widget=forms.Textarea, required=False)
+    address_eng = forms.CharField(label='Адрес на английском', widget=forms.Textarea, required=False)
 
     class Meta:
         model = Branche
